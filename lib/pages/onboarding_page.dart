47da75111ee6741e008e81a8ab5f26e611ef6792
Artena <artenadhytyan22@gmail.com>
2023-01-20 11:43:59 +0700

    add onboarding page

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -1,71 +1,62 @@
-// ignore_for_file: prefer_const_constructors, duplicate_ignore, use_key_in_widget_constructors, sized_box_for_whitespace
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
           Container(
-            // ignore: prefer_const_constructors
             decoration: BoxDecoration(
-              // ignore: prefer_const_constructors
               image: DecorationImage(
-                image: AssetImage(
-                  'assets/images/bg.png',
-                ),
+                image: AssetImage('assets/images/bg.png'),
                 fit: BoxFit.cover,
-              ),
+                )
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(
-              top: 70,
-              right: 30,
-              left: 30,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            padding: const EdgeInsets.only(top: 80, right: 30, left: 30),
+            child: Column( crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  child: Text(
-                    'Build Your Next \nFuture Career Like \na Master',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
+                Text('Build Your Next Future Career Like a Master',
+                style: GoogleFonts.poppins(
+                  color: Color(0xffffffff),
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
                 ),
-                SizedBox(height: 20),
-                Text(
-                  '18,000 jobs available',
-                  style: GoogleFonts.poppins(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w300,
-                  ),
+                ),
+                SizedBox(height: 20,),
+                Text('18.000 Jobs Available',
+                style: GoogleFonts.poppins(
+                  color: Color(0xffffffff),
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300,
+                ),
                 ),
                 Center(
                   child: Padding(
-                    padding: const EdgeInsets.only(top: 500),
+                    padding: const EdgeInsets.only(top:550),
                     child: Container(
-                      width: 200,
+                      width: 450,
                       height: 45,
                       child: TextButton(
                         style: TextButton.styleFrom(
-                            backgroundColor: Color(0xffFFFFFF),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(66))),
-                        onPressed: () {},
+                          backgroundColor: Color(0xffffffff),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40),
+                          )
+                        ),
+                        onPressed: () {}, 
                         child: Text(
                           'Get Started',
                           style: GoogleFonts.poppins(
-                            color: Color(0xff4141A4),
-                            fontSize: 14,
+                            color: Color(0xff4141a4),
+                            fontSize: 16,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
@@ -75,41 +66,32 @@ class OnboardingPage extends StatelessWidget {
                 ),
                 Center(
                   child: Padding(
-                    padding: const EdgeInsets.only(top: 16.0),
+                    padding: const EdgeInsets.only(top: 16),
                     child: Container(
-                      width: 200,
+                      width: 450,
                       height: 45,
                       child: OutlinedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => OnboardingPage()),
-                          );
-                        },
                         style: OutlinedButton.styleFrom(
-                            side: BorderSide(
-                              color: Colors.white,
-                            ),
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(66))),
-                        child: Text(
-                          'Sign In',
-                          style: GoogleFonts.poppins(
-                            color: Colors.white,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
+                          side: BorderSide(color: Color(0xffffffff), width: 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40),
                           ),
                         ),
-                      ),
+                        onPressed: () {},
+                         child: Text('Sign In',
+                         style: GoogleFonts.poppins(
+                          color: Color(0xffffffff),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                         ),)),
                     ),
                   ),
                 )
               ],
             ),
           )
-        ],
-      ),
+        ]
+        )
     );
   }
 }
---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: implementation_imports
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/container.dart';
+// import 'package:material/flutter.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({super.key});
+
+  @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
