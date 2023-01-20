fd75e72505a35c0d11428c9683db78ea7e91e49a
Artena <artenadhytyan22@gmail.com>
2023-01-21 00:04:31 +0700

    add text style

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: Color(0xffB3B5C4),
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+);
+
+TextStyle subTtitleTextStyle = GoogleFonts.poppins(
+  color: Color(0xff272C2F),
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
